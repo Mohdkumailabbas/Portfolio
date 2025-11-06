83e7ec1ef2dc65fcd00d08150316154cdba08b09
Mohdkumailabbas <kumailabbasbbk@gmail.com>
2025-11-06 08:16:25 +0530

    4  commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,10 @@
 // next.config.mjs
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export', // for static export
+  // output: 'export', // for static export
+   images: {
+    unoptimized: true, // disables Next.js image optimization
+  },
 };
 
 export default nextConfig;
